eac58401afc2a263a8136701f8c09d53c0cdcc0e
Mojtaba Tamizi <mtamizi1378@gmail.com>
2023-12-28 00:24:31 +0330

    start working on Transformer

---
+++ Transformer/model.py
@@ -0,0 +1,64 @@
+import torch
+import torch.nn as nn
+import math
+
+
+class InputEmbeddings(nn.Module):
+    
+    def __init__(self, d_model:int, vocab_size:int):
+        super().__init__()
+        self.d_model = d_model
+        self.vocab_size = vocab_size
+        self.embedding = nn.Embedding(vocab_size, d_model)
+
+    def forward(self, x):
+        return self.embedding(x) * math.sqrt(self.d_model)
+    
+
+class PositionalEncoding(nn.Module):
+    def __init__(self, d_model: int, seq_len: int, dropout: float) -> None:
+        super().__init__()
+        self.d_model = d_model
+        self.seq_len = seq_len
+        self.dropout = nn.Dropout(self.dropout)
+        #create a matrix of shape (seq_len, d_model)
+        pe = torch.zeros(self.seq_len, self.d_model)
+        #create a vector of shape (seq_len, 1)
+        possition = torch.arange(0, self.seq_len, dtype=torch.float).unsqueeze(1)
+        div_term = torch.exp(torch.arange(0, self.d_model, 2).float() * (-math.log(10000.0)/ d_model))
+        pe[:, 0::2] = torch.sin(possition * div_term)
+        pe[:, 1::2] = torch.cos(possition * div_term)
+        pe = pe.unsqueeze(0)
+
+        self.register_buffer('pe', pe)
+    
+    def forward(self, x):
+        x = x + (self.pe[:, :x.shape[1], :]).requires_grad_(False)
+        return self.dropout(x)
+    
+
+class LayerNormalizaiton(nn.Module):
+    def __init__(self, eps: float = 10**-6) -> None:
+        super().__init__()
+        self.eps = eps
+        self.alpha = nn.Parameter(torch.ones(1))
+        self.bias = nn.Parameter(torch.zeors(1))
+
+    def forward(self, x):
+        mean = x.mean(dim = -1, keepdim=True)
+        std = x.std(dim = -1, keepdim=True)
+        return self.alpha * (x - mean) / (std + self.eps) + self.bias
+
+
+class FFN(nn.Module):
+    def __init__(self, d_model: int, d_ff: int, dropout: float) -> None:
+        super().__init__()
+        self.linear_1 = nn.Linear(d_model, d_ff)
+        self.dropout = nn.Dropout(dropout)
+        self.linear_2 = nn.Linear(d_ff, d_model)
+
+
+    def forward(self, x):
+        return self.linear_2(self.dropout(torch.relu(self.linear_1(x))))
+    
+
